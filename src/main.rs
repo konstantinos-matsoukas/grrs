ac602dd1621fb98d91c9d70d150fd32daecb6a5d
konstantinos-matsoukas <kostasmats9@gmail.com>
2023-11-30 16:05:28 +0200

    begin

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+        find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+        
+
+    
+
+    Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            if let Err(err) = writeln!(writer, "{}", line) {
+                eprintln!("Error writing line: {}", err);
+            }
+        }
+    }
+}
